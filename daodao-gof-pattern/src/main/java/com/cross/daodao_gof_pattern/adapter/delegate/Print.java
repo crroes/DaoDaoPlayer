2a6cb63e16766e973fb3cab790ffca4782d14dda
liuyan <crrose@aliyun.com>
2018-07-31 14:31:19 +0800

    设计模式二 Adapter 1/4

---
+++ daodao-gof-pattern/src/main/java/com/cross/daodao_gof_pattern/adapter/delegate/Print.java
@@ -0,0 +1,11 @@
+package com.cross.daodao_gof_pattern.adapter.delegate;
+
+/**
+ * Created by cross on 2018/7/31.
+ * <p>描述:适配器（包装）模式核心（1/4）目标接口
+ */
+
+public interface Print {
+	void printWeak();
+	void printStrong();
+}
