79de8aac03ffe8b719dc2a0ffa45b3cc48aaae64
= <=>
2018-08-21 15:48:06 +0800

    本地分支提交测试

--- daodao-widget-gof/src/test/java/com/cross/daodao_gof_pattern/PatternTest.java
+++ daodao-widget-gof/src/test/java/com/cross/daodao_gof_pattern/PatternTest.java
@@ -21,6 +21,7 @@ public class PatternTest {
 		bookShelf.appendBook(new Book("Bible"));
 		bookShelf.appendBook(new Book("Cinderella"));
 		bookShelf.appendBook(new Book("Daddy Long Legs"));
+		bookShelf.appendBook(new Book("测试Git创建、合并分支"));
 		Iterator<Book> iterator = bookShelf.iterator();
 		while (iterator.hasNext()) {
 			Book next = iterator.next();
